52c8c7a09ece5df98f78fc47a434df9967ba688a
Mario <mni0000@alu.ubu.es>
2018-11-15 23:33:29 +0100

    Implementacion de los recursos basicos de la gui (mains).
    #8

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,13 +1,33 @@
 package gui;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+	private Stage primaryStage;
+	private static BorderPane mainLayout;
+
 	@Override
-	public void start(Stage primaryStage) {
-		
+	public void start(Stage primaryStage) throws IOException {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Menu principal");
+
+		showMain();
+	}
+
+	private void showMain() throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("view/MainView.fxml"));
+		Main.mainLayout = loader.load();
+		Scene scene = new Scene(mainLayout);
+		this.primaryStage.setScene(scene);
+		this.primaryStage.show();
 	}
 
 	public static void main(String[] args) {
