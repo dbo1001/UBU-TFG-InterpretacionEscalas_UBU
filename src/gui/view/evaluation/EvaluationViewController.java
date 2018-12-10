f61c796f7e68766b16fd7e203213c92e16ae5066
Mario <mni0000@alu.ubu.es>
2018-12-10 23:23:22 +0100

    Agregados los botones anterior y siguiente a la vista de evaluación.
    #12

--- src/gui/view/evaluation/EvaluationViewController.java
+++ src/gui/view/evaluation/EvaluationViewController.java
@@ -27,64 +27,70 @@ import model.Categorizacion;
 import model.Item;
 
 public class EvaluationViewController extends Controller {
-	
+
 	@FXML
 	TabPane tabPane;
-	
+
 	List<Areafuncional> allFunctionalAreas;
 	List<Categorizacion> allCategories;
 	List<Item> allItems;
 
 	private void loadData() {
-		
+
 		for (Areafuncional aF : this.allFunctionalAreas) {
 			int i = 0;
 			Tab tab = new Tab(aF.getDescripcion());
 			ScrollPane scrollPane = new ScrollPane();
 			scrollPane.setPadding(new Insets(15, 20, 0, 20));
 			GridPane gridPane = new GridPane();
-			/*AnchorPane anchorPane = new AnchorPane();
-			AnchorPane.setRightAnchor(gridPane, 0.0);
-			AnchorPane.setLeftAnchor(gridPane, 0.0);
-			AnchorPane.setTopAnchor(gridPane, 0.0);
-			gridPane.setGridLinesVisible(true);
-			anchorPane.getChildren().add(gridPane);*/
-			scrollPane.setContent(gridPane);
-			//scrollPane.setFitToWidth(true);
-			//scrollPane.setPrefSize(ScrollPane.USE_COMPUTED_SIZE, ScrollPane.USE_COMPUTED_SIZE);
+			Text advise = new Text(
+					"**Se evaluará el grado de adquisición de la habilidad en una escala de tipo Likert de 1 a 5 donde 1 = nunca o nada 5 = todo o siempre.");
+			advise.getStyleClass().add("mediumText");
+			/*
+			 * AnchorPane anchorPane = new AnchorPane(); AnchorPane.setRightAnchor(gridPane,
+			 * 0.0); AnchorPane.setLeftAnchor(gridPane, 0.0);
+			 * AnchorPane.setTopAnchor(gridPane, 0.0); gridPane.setGridLinesVisible(true);
+			 * anchorPane.getChildren().add(gridPane);
+			 */
+			VBox scrollPaneContent = new VBox(advise, gridPane);
+			scrollPaneContent.setSpacing(15);
+			scrollPane.setContent(scrollPaneContent);
+			// scrollPane.setFitToWidth(true);
+			// scrollPane.setPrefSize(ScrollPane.USE_COMPUTED_SIZE,
+			// ScrollPane.USE_COMPUTED_SIZE);
 			tab.setContent(scrollPane);
-			for(Categorizacion cat : this.allCategories) {
-				if(aF.equals(cat.getAreafuncional())) {
+			for (Categorizacion cat : this.allCategories) {
+				if (aF.equals(cat.getAreafuncional())) {
 					VBox textVBox = new VBox();
 					textVBox.setSpacing(10);
 					VBox buttonsVBox = new VBox();
-					//AnchorPane columnAP = new AnchorPane();
-					//buttonsVBox.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+					// AnchorPane columnAP = new AnchorPane();
+					// buttonsVBox.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
 					buttonsVBox.setSpacing(10);
-					//columnAP.getChildren().add(buttonsVBox);
+					// columnAP.getChildren().add(buttonsVBox);
 					gridPane.add(textVBox, 0, i);
 					gridPane.add(buttonsVBox, 1, i);
-					//gridPane.add(columnAP, 1, i);
+					// gridPane.add(columnAP, 1, i);
 					gridPane.setVgap(30);
 					gridPane.setHgap(20);
-					//GridPane.setHgrow(columnAP, Priority.ALWAYS);
-					//AnchorPane.setRightAnchor(buttonsVBox, 0.0);
-					
+					// GridPane.setHgrow(columnAP, Priority.ALWAYS);
+					// AnchorPane.setRightAnchor(buttonsVBox, 0.0);
+
 					Text catTitle = new Text(cat.getDescripcion());
 					catTitle.getStyleClass().add("bigText");
 					textVBox.getChildren().add(catTitle);
 					HBox scoresHBox = new HBox();
 					scoresHBox.setSpacing(33);
-					scoresHBox.setPadding(new Insets(0,0,0,4));
+					scoresHBox.setPadding(new Insets(0, 0, 0, 4));
 					for (int c = 1; c < 6; c++) {
 						Text score = new Text("" + c);
 						score.getStyleClass().add("mediumText");
 						scoresHBox.getChildren().add(score);
 					}
-					
+
 					buttonsVBox.getChildren().add(scoresHBox);
-					for(Item item : this.allItems) {
-						if(item.getCategorizacion().equals(cat)) {
+					for (Item item : this.allItems) {
+						if (item.getCategorizacion().equals(cat)) {
 							Text itemText = new Text(item.getNumero() + ". " + item.getDescripcion());
 							itemText.getStyleClass().add("smallText");
 							textVBox.getChildren().add(itemText);
@@ -93,7 +99,7 @@ public class EvaluationViewController extends Controller {
 							rbHBox.setSpacing(20);
 							ToggleGroup toggle = new ToggleGroup();
 							toggle.setUserData(item.getNumero());
-							for(int c = 0; c < 5; c++) {
+							for (int c = 0; c < 5; c++) {
 								RadioButton rB = new RadioButton();
 								rB.setToggleGroup(toggle);
 								rbHBox.getChildren().add(rB);
@@ -107,19 +113,30 @@ public class EvaluationViewController extends Controller {
 			tabPane.getTabs().add(tab);
 		}
 	}
-	
-	public void setData (List<Areafuncional> allFunctionalAreas, List<Categorizacion> allCategories, List<Item> allItems) {
+
+	public void setData(List<Areafuncional> allFunctionalAreas, List<Categorizacion> allCategories,
+			List<Item> allItems) {
 		this.allFunctionalAreas = allFunctionalAreas;
 		this.allCategories = allCategories;
 		this.allItems = allItems;
 		this.loadData();
 	}
-	
+
 	@FXML
 	private void cancel() throws IOException {
-		if(this.cancelAlert()) {
+		if (this.cancelAlert()) {
 			Main.showManageView();
 		}
 	}
 	
+	@FXML
+	private void next() {
+		this.tabPane.getSelectionModel().selectNext();
+	}
+	
+	@FXML
+	private void previous() {
+		this.tabPane.getSelectionModel().selectPrevious();
+	}
+
 }
